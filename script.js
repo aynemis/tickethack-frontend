56636a80e8ead6d95b4e9cba16fd99e276a3a490
Your name <Your email>
2023-11-07 21:33:22 +0100

    ajout du bouton Book + styles

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-var moment = require('moment');
-
 document.querySelector('#search-button').addEventListener('click', function(){
     fetch('http://localhost:3000/trip',{
         method: 'POST',
@@ -14,20 +12,22 @@ document.querySelector('#search-button').addEventListener('click', function(){
 .then(data => {
     console.log(data)
     if (data.result) {
+        document.querySelector('#visualisation').innerHTML =''
         let trips = data.trips
         for (let i=0; i<trips.length ; i++){
-            let time = moment(trips[i].date).format("hh:mm:ss a");
-            console.log(trips[i]);
-            document.querySelector('#visualisation').innerHTML = `
+            let time = moment(trips[i].date).format("hh:mm");
+            console.log(time);
+            document.querySelector('#visualisation').innerHTML += `
             <div class="journey"> 
-            <p>${trips[i].departure}>${trips[i].arrival}</p> 
-            <p>${time}</p>
-            <p>${trips[i].price}</p>
-            <p>Departure in ... hours</p>
+            <div> <p>${trips[i].departure}>${trips[i].arrival}</p> </div>
+            <div> <p>${time}</p> </div>
+            <div> <p>${trips[i].price}€</p> </div>
+            <div> <p>Departure in ... hours</p> </div>
+            <div> <button class="book-btn">Book</button></div>
             </div>`
         }
     }else if(data.result === false){
-        document.querySelector('#logo').src = '../images/notfound.png'
+        document.querySelector('#logo').src = '/Frontend/images/notfound.png'
         document.querySelector('#text-logo').textContent = 'No trip found.'
     }
 
