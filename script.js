5953a6f498dd1613dbfeadf27af63fc363b050d4
Your name <Your email>
2023-11-07 19:15:00 +0100

    Search button add event listener

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
+var moment = require('moment');
+
 document.querySelector('#search-button').addEventListener('click', function(){
-    fetch('http//localhost:3000/trip',{
-        method: 'GET',
+    fetch('http://localhost:3000/trip',{
+        method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
             departure: document.querySelector('#departure').value,
             arrival: document.querySelector('#arrival').value,
-            date: document.querySelector('#date').value,
+            date: document.querySelector('#date-selection').value,
     })
 })
 .then(response => response.json())
 .then(data => {
+    console.log(data)
     if (data.result) {
-        for (let i=0; i<data.trips.length ; i++){
-            document.querySelectorAll('.trips-results').textContent = data.trips[i]
+        let trips = data.trips
+        for (let i=0; i<trips.length ; i++){
+            let time = moment(trips[i].date).format("hh:mm:ss a");
+            console.log(trips[i]);
+            document.querySelector('#visualisation').innerHTML = `
+            <div class="journey"> 
+            <p>${trips[i].departure}>${trips[i].arrival}</p> 
+            <p>${time}</p>
+            <p>${trips[i].price}</p>
+            <p>Departure in ... hours</p>
+            </div>`
         }
-    }else if(data.result == false){
-        document.querySelector('#trips-image').src = '../images/notfound.png'
-        document.querySelector('#trips-text').textContent = 'No trip found.'
+    }else if(data.result === false){
+        document.querySelector('#logo').src = '../images/notfound.png'
+        document.querySelector('#text-logo').textContent = 'No trip found.'
     }
 
 })
