0fe7fc7523b6124539e67bee9ff6d619d7c3332f
Your name <Your email>
2023-11-08 14:42:10 +0100

    cart purchase

--- cart.js
+++ cart.js
@@ -1,7 +1,8 @@
 fetch("http://localhost:3000/selectedtrips/cart")
 .then(response => response.json())
 .then(data => {
-    let trips = data.trips
+    let trips = data.trips;
+    if(trips.length>0){
     for(let i=0; i<trips.length ; i++){
         document.querySelector("#cart").innerHTML+=`
         <div class="journey-cart"> 
@@ -12,5 +13,30 @@ fetch("http://localhost:3000/selectedtrips/cart")
         </div>
         `
     }
+}else{
+    document.querySelector("#cart").innerHTML+=`
+        <p>No journeys in your cart!<p>`
+}
+
+    purchase()
 })
+
+function purchase (){
+document.querySelector("#purchase-btn").addEventListener("click", function(){
+
+    if(document.querySelectorAll(".journey-cart").length>0){
+        fetch("http://localhost:3000/selectedtrips/bookings",{
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(()=> {
+            console.log("Purchase OK")
+            window.location.assign("http://127.0.0.1:5500/Frontend/booking.html")
+        })
+    }else{
+        document.querySelector("#cart").innerHTML=`
+        <p>No journeys in your cart! Please add a journey.<p>`
+    }
+})
+}
 
